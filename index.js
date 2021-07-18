60fd5772002bc76cda6dced4beeb5244be241359
Jacobo Rodriguez <jrvdaw@gmail.com>
2021-07-18 10:41:50 +0000

    Ejercicio 3.5

--- index.js
+++ index.js
@@ -37,6 +37,24 @@ app.get('/api/persons/:id', (req, res) => {
     person ? res.json(person) : res.status(404).end()
 })
 
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    if(!body.name) {
+        return res.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number || ''
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
+})
+
 app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(p => p.id !== id)
@@ -49,6 +67,12 @@ app.get('/info', (request, response) => {
         + '<p>' + new Date() + '</p>'
     )
 })
+
+const generateId = () => {
+    const min = Math.ceil(100);
+    const max = Math.floor(200);
+    return Math.floor(Math.random() * (max - min) + min)
+}
 
 const PORT = 3001
 app.listen(PORT)
